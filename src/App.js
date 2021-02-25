7801ede785feff4dff9e549e4c47527af3bab723
Jorge Palacios <reefaktor@gmail.com>
2021-02-25 19:47:22 -0300

    Implemented basic template with Bulma.io
    * Implemented template
    * Created Users view
    * Created Tasks view
    * Created Router placeholder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Users from './views/Users';
+import Tasks from './views/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <view className='container'>
+      <view className='columns'>
+        <header className='column is-10 is-offset-1 has-text-centered'>
+          <h1 className='title is-1'>TO-DO APP</h1>
+        </header>
+      </view>
+
+      {/* Here starts the app */}
+      <Users />
+
+      <h1>.</h1>
+      <h1>.</h1>
+      <Tasks />
+
+      {/* Footer */}
+      <footer className='footer'>
+        <div className='content has-text-centered column is-6 is-offset-3'>
+          <p>
+            <strong>TO-DO APP</strong> by{' '}
+            <a href='https://jorge-codes.com'>Jorge Palacios</a>. The source
+            code is licensed{' '}
+            <a href='http://opensource.org/licenses/mit-license.php'>MIT</a>.
+            The website content is licensed{' '}
+            <a href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
+              CC BY NC SA 4.0
+            </a>
+            .
+          </p>
+        </div>
+      </footer>
+    </view>
   );
 }
 
---
+++ src/components/Router.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      {/* <Route exact path='' component={} /> */}
+      {/* <Route exact path='/user/:userId' component={} /> */}
+      {/* <Route component={NotFound} /> */}
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
