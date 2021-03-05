b28c974e2e71be9fabe5d2c87aaceebe91fcfcc1
Jorge Palacios <reefaktor@gmail.com>
2021-03-04 22:10:07 -0300

    Ported UserForm to functional component
    * Ported UserForm to functional component pattern
    * Refactored and fixed the API calls to promise pattern

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,47 +1,51 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-class UserForm extends React.Component {
-  inputRef = React.createRef();
+const UserForm = (props) => {
+  const [inputText, setInputText] = useState('');
+  const inputRef = useRef(null);
 
-  submitHandler = (event) => {
+  const inputHandler = (event) => {
+    setInputText(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    this.props.addUser(name);
+    const name = inputText;
+    props.addUser(name);
     event.currentTarget.reset();
   };
 
-  componentDidMount() {
-    this.inputRef.current.focus();
-  }
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-  render() {
-    return (
-      <React.Fragment>
-        {/* UserForm */}
-        <form onSubmit={this.submitHandler}>
-          <div className='columns'>
-            <div className='column is-8 is-offset-2 has-text-centered'>
-              <div className='field has-addons is-expanded'>
-                <div className='control is-expanded'>
-                  <input
-                    ref={this.inputRef}
-                    className='input'
-                    type='text'
-                    placeholder='Add new user here'
-                  />
-                </div>
-                <div className='control'>
-                  <button className='button is-primary has-icons' type='submit'>
-                    <i className='fas fa-plus'></i>
-                  </button>
-                </div>
+  return (
+    <React.Fragment>
+      {/* UserForm */}
+      <form onSubmit={submitHandler}>
+        <div className='columns'>
+          <div className='column is-8 is-offset-2 has-text-centered'>
+            <div className='field has-addons is-expanded'>
+              <div className='control is-expanded'>
+                <input
+                  ref={inputRef}
+                  onChange={inputHandler}
+                  className='input'
+                  type='text'
+                  placeholder='Add new user here'
+                />
+              </div>
+              <div className='control'>
+                <button className='button is-primary has-icons' type='submit'>
+                  <i className='fas fa-plus'></i>
+                </button>
               </div>
             </div>
           </div>
-        </form>
-      </React.Fragment>
-    );
-  }
-}
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
 
 export default UserForm;
